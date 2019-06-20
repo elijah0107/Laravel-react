73d8ecb046ee65a0751af4d2958ca95a2b12c575
gayov_ra <gayov_ra@ekb.sima-land.ru>
2019-06-20 17:55:25 +0500

    Отправка сообщений в телеграм

--- app/Http/Controllers/NoticeController.php
+++ app/Http/Controllers/NoticeController.php
@@ -8,24 +8,34 @@ use App\Model\Notice;
 class NoticeController extends Controller
 {
 
-    public function insert(Request $request)
-    {
-        $notice = new Notice;
-        $email = $request->input('email');
-        $emailExist = empty($notice::where('email', 'LIKE', $email)->get()->toArray());
-        if(!$emailExist) {
-            return "Мы уже отправляли файл на эту почту, пожалуйста проверьте в своих сообщениях";
-        }
-        $notice->email = $email;
-        $notice->name = $request->input('name');
-        $result = $notice->save();
-
-        if ($result == 1) {
-            return "Файл успешно отправлен вам на почту, если не пришло проверьте папку спам";
-        }
-        else {
-            return "Что-то пошло не так";
-        }
-
+  public function insert(Request $request)
+  {
+    $token = '1232434';
+    $chat_id = '2132142';
+    $notice = new Notice;
+    $email = $request->input('email');
+    $name = $request->input('name');
+    $emailExist = empty($notice::where('email', 'LIKE', $email)->get()->toArray());
+    if (!$emailExist) {
+      return "Мы уже отправляли файл на эту почту, пожалуйста проверьте в своих сообщениях";
     }
+    $notice->email = $email;
+    $notice->name = $request->input('name');
+    $arr = array(
+      'Имя' => $email,
+      'Почта' => $name,
+    );
+    $result = $notice->save();
+    $txt = '';
+    foreach ($arr as $key => $value) {
+      $txt .= "<b>".$key."</b> ".$value."%0A";
+    }
+    if ($result == 1) {
+      $sendMessageToTelegram = fopen("https://api.telegram.org/bot($token)/sendMessage?chat_id=($chat_id)&parse_mode=html&text={$txt}", '');
+      return "Файл успешно отправлен вам на почту, если не пришло проверьте папку спам";
+    } else {
+      return "Что-то пошло не так";
+    }
+
+  }
 }
