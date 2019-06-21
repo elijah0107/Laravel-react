d3c377d565f80c3cbe4b067fe977bc5bac76a476
gayov_ra <gayov_ra@ekb.sima-land.ru>
2019-06-21 17:35:51 +0500

    правки по телеграмму

--- app/Http/Controllers/NoticeController.php
+++ app/Http/Controllers/NoticeController.php
@@ -31,11 +31,26 @@ class NoticeController extends Controller
       $txt .= "<b>".$key."</b> ".$value."%0A";
     }
     if ($result == 1) {
-        $tbot = file_get_contents("https://api.telegram.org/bot".$token."/sendMessage?chat_id=".$chat_id."&text=".urlencode($txt));
+      $this->sendMessage($chat_id, 'Тест тест', $token);
       return "Файл успешно отправлен вам на почту, если не пришло проверьте папку спам";
     } else {
       return "Что-то пошло не так";
     }
 
+  }
+
+  function sendMessage($chatID, $messaggio, $token) {
+
+    $url = "https://api.telegram.org/bot" . $token . "/sendMessage?chat_id=" . $chatID;
+    $url = $url . "&text=" . urlencode($messaggio);
+    $ch = curl_init();
+    $optArray = array(
+      CURLOPT_URL => $url,
+      CURLOPT_RETURNTRANSFER => true
+    );
+    curl_setopt_array($ch, $optArray);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    return $result;
   }
 }
